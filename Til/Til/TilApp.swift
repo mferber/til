ee0f972c847de81a38c889480806d73be0217803
Matthias Ferber <matthias.ferber@gmail.com>
2021-09-06 16:36:54 -0400

    Create empty Xcode project

---
+++ Til/Til/TilApp.swift
@@ -0,0 +1,17 @@
+//
+//  TilApp.swift
+//  Til
+//
+//  Created by Matthias Ferber on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
