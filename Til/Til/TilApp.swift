ef3f83251b6136f0ce2c26d6c1dd13b17799deaf
Matthias Ferber <matthias.ferber@gmail.com>
2021-09-06 17:13:17 -0400

    Clean out project skeleton

--- Til/Til/TilApp.swift
+++ Til/Til/TilApp.swift
@@ -1,17 +1,9 @@
-//
-//  TilApp.swift
-//  Til
-//
-//  Created by Matthias Ferber on 9/6/21.
-//
-
 import SwiftUI
 
 @main
 struct TilApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
     }
+  }
 }
--- Til/TilTests/TilTests.swift
+++ Til/TilTests/TilTests.swift
@@ -1,33 +1,15 @@
-//
-//  TilTests.swift
-//  TilTests
-//
-//  Created by Matthias Ferber on 9/6/21.
-//
-
-import XCTest
+import Nimble
+import Quick
 @testable import Til
 
-class TilTests: XCTestCase {
+class TilAppSpec: QuickSpec {
+  override func spec() {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    describe("test suite") {
+
+      it("runs tests") {
+        expect(true).to(beTrue())
+      }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
 }
